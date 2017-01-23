fc7ca27a27ca42c5f3baba40d38344f69df77b8e
Tyler Rose <tylerjosephrose@gmail.com>
2017-01-23 16:52:10 -0500

    Add labels for rgba values

--- Rose_ColorPallette/ViewController.swift
+++ Rose_ColorPallette/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 	
+	@IBOutlet weak var redLbl: UILabel!
+	@IBOutlet weak var greenLbl: UILabel!
+	@IBOutlet weak var blueLbl: UILabel!
+	@IBOutlet weak var alphaLbl: UILabel!
 	@IBOutlet weak var redSlider: UISlider!
 	@IBOutlet weak var blueSlider: UISlider!
 	@IBOutlet weak var alphaSlider: UISlider!
@@ -23,8 +27,15 @@ class ViewController: UIViewController {
 	@IBOutlet weak var colorView: UIView!
 
 	@IBAction func displayColor(_ sender: UISlider) {
+		// Set UIView background color
 		let userColor = UIColor(colorLiteralRed: redSlider.value, green: greenSlider.value, blue: blueSlider.value, alpha: alphaSlider.value)
 		colorView.backgroundColor = userColor
+		
+		// Display the color values of each slider
+		redLbl.text = String.localizedStringWithFormat("%0.3f", redSlider.value)
+		greenLbl.text = String.localizedStringWithFormat("%0.3f", greenSlider.value)
+		blueLbl.text = String.localizedStringWithFormat("%0.3f", blueSlider.value)
+		alphaLbl.text = String.localizedStringWithFormat("%0.3f", alphaSlider.value)
 	}
 
 }
