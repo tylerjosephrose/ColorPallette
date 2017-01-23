f07c500d7e9b39eaf3f8de2619d28f1bcd8f4484
Tyler Rose <tylerjosephrose@gmail.com>
2017-01-23 13:24:31 -0500

    Added red slider and UIView for preview

--- Rose_ColorPallette/ViewController.swift
+++ Rose_ColorPallette/ViewController.swift
@@ -14,12 +14,12 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
+	@IBOutlet weak var redSlider: UISlider!
+	@IBOutlet weak var colorView: UIView!
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func displayColor(_ sender: UISlider) {
 	}
-
 
 }
 
