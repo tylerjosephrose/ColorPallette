7dc8343bad09662e4820ab9be6cbb0c21e82fefd
Tyler Rose <tylerjosephrose@gmail.com>
2017-01-23 16:32:06 -0500

    Add other sliders

--- Rose_ColorPallette/ViewController.swift
+++ Rose_ColorPallette/ViewController.swift
@@ -16,14 +16,15 @@ class ViewController: UIViewController {
 	}
 	
 	@IBOutlet weak var redSlider: UISlider!
-	let greenSlider: Float = 0
-	let blueSlider: Float = 0
-	let alphaSlider: Float = 1
+	@IBOutlet weak var blueSlider: UISlider!
+	@IBOutlet weak var alphaSlider: UISlider!
+	@IBOutlet weak var greenSlider: UISlider!
+
 	@IBOutlet weak var colorView: UIView!
 
 	@IBAction func displayColor(_ sender: UISlider) {
-		let color = UIColor(colorLiteralRed: redSlider.value, green: greenSlider, blue: blueSlider, alpha: alphaSlider)
-		colorView.backgroundColor = color
+		let userColor = UIColor(colorLiteralRed: redSlider.value, green: greenSlider.value, blue: blueSlider.value, alpha: alphaSlider.value)
+		colorView.backgroundColor = userColor
 	}
 
 }
